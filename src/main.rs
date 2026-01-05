5b8e59b66b91f69af88223e383b5eb995db1f9f9
Egevtech <egevtech@gmail.com>
2026-01-05 23:10:19 +0300

    few code improvements

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,17 @@ use std::process::{Command};
 const SCRAP_TEMPLATE: &str = r#"
 @@@@@@@@@::.....:@@@    {hostname}
 @: :..... ......:@@@    =========
-:  ....:-===++++@@@@    OS - {osname}
-:. .=++++++++===::@@    Kernel - {kernelinfo}
+:  ....:-===++++@@@@    OS - {sysname}
+:. .=++++++++===::@@    Kernel - {kernel}
 @.  :+===-..... ..:@
-@ .. .....:-==+:  .@    Mem - {mem_used}/{mem_total} MB
-@@..====+++++++=. .:    Swap - {swap_used}/{swap_total} MB
+@ .. .....:-==+:  .@    Mem - {mem_use}/{mem_total} MB
+@@..====+++++++=. .:    Swap - {swap_use}/{swap_total} MB
 @@@@++=+==--:.... .:
-@@@::..... ......:@@    Scrap is {status} on your {osname}
+@@@::..... ......:@@    Scrap is {scrap_status} on your {sysname}
 @@@:... ..:@@@@@@@@@
 "#;
 
 fn main() {
-    let mut info = HashMap::new();
     let mut system = System::new_all();
 
     let output = Command::new("which")
@@ -29,16 +28,16 @@ fn main() {
 
     system.refresh_all();
 
-    info.insert("hostname".to_string(), System::host_name().unwrap());
-    info.insert("osname".to_string(), System::name().unwrap());
-    info.insert("kernelinfo".to_string(), System::kernel_long_version());
-    info.insert("mem_used".to_string(), (system.used_memory() / (1024 * 1024)) .to_string());
-    info.insert("mem_total".to_string(), (system.total_memory() / (1024 * 1024)).to_string());
-    info.insert("swap_used".to_string(), (system.used_swap() / (1024 * 1024)).to_string());
-    info.insert("swap_total".to_string(), (system.total_swap() / (1024 * 1024)).to_string());
-    info.insert("status".to_string(), if output.status.code().unwrap() != 0 { "not installed".to_string() } else { "installed".to_string() });
+    let hostname = System::host_name().unwrap();
+    let sysname = System::name().unwrap();
+    let kernel = System::kernel_long_version();
+    let mem_use = system.used_memory() / (1024 * 1024);
+    let mem_total = system.total_memory() / (1024 * 1024);
+    let swap_use = system.used_swap() / (1024 * 1024);
+    let swap_total = system.total_swap() / (1024 * 1024);
+    let scrap_status = if output.status.code().unwrap() != 0 { "not installed".to_string() } else { "installed".to_string() };
 
-    let formatted = strfmt::strfmt(&SCRAP_TEMPLATE.to_string(), &info);
+    let formatted = strfmt::strfmt!(SCRAP_TEMPLATE, hostname, sysname.clone(), kernel, mem_use, mem_total, swap_use, swap_total, scrap_status, sysname);
 
     println!("{}", formatted.unwrap());
 }
